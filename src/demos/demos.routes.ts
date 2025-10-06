0ae31b0d03c63e2c59311ea454b935327dcc1193
adam <private>
2025-10-06 20:42:45 +0000

    refactor navbar and add demos

---
+++ src/demos/demos.routes.ts
@@ -0,0 +1,16 @@
+import { Routes } from '@angular/router';
+import { Demos } from './demos';
+import { Signals } from './pages/signals';
+
+export const DEMO_ROUTES: Routes = [
+  {
+    path: '',
+    component: Demos,
+    children: [
+      {
+        path: 'signals',
+        component: Signals,
+      },
+    ],
+  },
+];
